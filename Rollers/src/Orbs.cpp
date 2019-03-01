c36e70ed47a92cd551cdd4f60f50bd468ce58105
Alex Pavlak <alexander.pavlak@mavs.uta.edu>
2019-03-01 13:06:09 -0600

    Reworked Orbs to work with Items getters and setters. Compiles!

--- Rollers/src/Orbs.cpp
+++ Rollers/src/Orbs.cpp
@@ -2,14 +2,14 @@
 
 void Orbs::transmute(Item &item, std::vector<Mod> &rollingPrefixes, std::vector<Mod> &rollingSuffixes){
 
-    if(item.rarity != 1){
+    if(item.getRarity() != 1){
 
         std::cout << "Item is not normal, can not transmute." << std::endl;
         return;
     }
 
     unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
-    item.rarity = 2;
+    item.setRarity(2);
     std::default_random_engine generator(seed);
     std::uniform_int_distribution<int> oneOrTwo(1,2);
     std::uniform_int_distribution<int> prefixDist(0,rollingPrefixes.size() - 1);
